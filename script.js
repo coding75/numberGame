28afce7091ec134f4db17e656e09586b66f6ca15
coding75 <codingWeb09@gmail.com>
2024-12-03 16:02:21 +0530

    Final commit

--- script.js
+++ script.js
@@ -1,23 +1,62 @@
 'use strict';
 
-const number = Math.trunc(Math.random() * 20) + 1;
+let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(typeof guess);
+
   if (!guess) {
     document.querySelector('.message').textContent =
       'No number found in enter field';
   } else if (guess === number) {
     document.querySelector('.message').textContent = 'Correct Number';
-  } else if (guess > number) {
+    document.querySelector('.number').textContent = number;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30 rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== number) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Guess number is high';
+      document.querySelector('.message').textContent =
+        guess > number ? 'Guess number is high' : 'Guess number is low';
       score--;
       document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You loss the game';
+      document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < number) {
-    document.querySelector('.message').textContent = 'Guess number is low';
-    document.querySelector('.score').textContent = score;
   }
+
+  // else if (guess > number) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'Guess number is high';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = 'You loss the game';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // } else if (guess < number) {
+  //   if (score > 0) {
+  //     document.querySelector('.message').textContent = 'Guess number is low';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = 'You loss the game';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = 0;
+  document.querySelector('body').style.backgroundColor = ' #222';
+  document.querySelector('.guess').value = ' ';
 });
